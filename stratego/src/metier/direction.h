d3718231827e0ad93815315e4970e8389471a664
52949 <52949@etu.he2b.be>
2023-02-05 16:39:33 +0100

    end

---
+++ stratego/src/metier/direction.h
@@ -0,0 +1,20 @@
+/*|
+ * \file state.h
+ * \brief The direction a piece can move to
+ */
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+
+namespace stratego {
+/**
+* @brief The direction enum represents the direction of the movement.
+*/
+
+enum class Direction {
+    UP, DOWN, LEFT, RIGHT
+};
+
+}
+
+#endif // DIRECTION_H
